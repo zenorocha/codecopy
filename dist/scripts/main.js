29416fc2c22330cf5c9a0fca1a600123b1c207cc
Zeno Rocha <zno.rocha@gmail.com>
2017-04-30 17:10:20 -0700

    Build

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -773,11 +773,15 @@ var _clipboard = require('clipboard');
 
 var _clipboard2 = _interopRequireDefault(_clipboard);
 
+var _util = require('./util');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// Scan for code snippets and append buttons
 
 var snippets = document.querySelectorAll('pre');
 
-[].forEach.call(snippets, function (snippet) {
+snippets.forEach(function (snippet) {
   var parent = snippet.parentNode;
   var wrapper = document.createElement('div');
 
@@ -785,14 +789,14 @@ var snippets = document.querySelectorAll('pre');
   wrapper.appendChild(snippet);
 
   wrapper.classList.add('codecopy');
-  wrapper.firstChild.insertAdjacentHTML('beforebegin', '<button class="codecopy-btn tooltipped tooltipped-s" aria-label="Copy to clipboard"><svg height="16" class="codecopy-btn-icon" viewBox="0 0 14 16" version="1.1" width="16" aria-hidden="true"><path fill-rule="evenodd" d="M2 13h4v1H2v-1zm5-6H2v1h5V7zm2 3V8l-3 3 3 3v-2h5v-2H9zM4.5 9H2v1h2.5V9zM2 12h2.5v-1H2v1zm9 1h1v2c-.02.28-.11.52-.3.7-.19.18-.42.28-.7.3H1c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h3c0-1.11.89-2 2-2 1.11 0 2 .89 2 2h3c.55 0 1 .45 1 1v5h-1V6H1v9h10v-2zM2 5h8c0-.55-.45-1-1-1H8c-.55 0-1-.45-1-1s-.45-1-1-1-1 .45-1 1-.45 1-1 1H3c-.55 0-1 .45-1 1z"></path></svg></button>');
+  wrapper.firstChild.insertAdjacentHTML('beforebegin', _util.htmlButton);
 
-  var sites = /^(github.com|gist.github.com|medium.com|www.npmjs.com|developer.mozilla.org)$/;
-
-  if (sites.exec(document.location.hostname)) {
+  if ((0, _util.isLargeButton)()) {
     wrapper.classList.add('codecopy-lg');
   }
 });
+
+// Add copy to clipboard functionality and user feedback
 
 var clipboard = new _clipboard2.default('.codecopy-btn', {
   target: function target(trigger) {
@@ -801,22 +805,32 @@ var clipboard = new _clipboard2.default('.codecopy-btn', {
 });
 
 clipboard.on('success', function (e) {
+  e.trigger.setAttribute('aria-label', 'Copied!');
   e.clearSelection();
-
-  showTooltip(e.trigger, 'Copied!');
 });
+
+// Replace tooltip message when mouse leaves button
 
 var btns = document.querySelectorAll('.codecopy-btn');
 
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener('mouseleave', function (e) {
-    showTooltip(e.target, 'Copy to clipboard');
+btns.forEach(function (btn) {
+  btn.addEventListener('mouseleave', function (e) {
+    e.target.setAttribute('aria-label', 'Copy to clipboard');
     e.target.blur();
   });
-}
+});
 
-function showTooltip(elem, msg) {
-  elem.setAttribute('aria-label', msg);
-}
+},{"./util":10,"clipboard":2}],10:[function(require,module,exports){
+"use strict";
 
-},{"clipboard":2}]},{},[9]);
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var htmlButton = exports.htmlButton = "\n  <button class=\"codecopy-btn tooltipped tooltipped-s\" aria-label=\"Copy to clipboard\">\n    <svg height=\"16\" class=\"codecopy-btn-icon\" viewBox=\"0 0 14 16\" version=\"1.1\" width=\"16\" aria-hidden=\"true\">\n      <path fill-rule=\"evenodd\" d=\"M2 13h4v1H2v-1zm5-6H2v1h5V7zm2 3V8l-3 3 3 3v-2h5v-2H9zM4.5 9H2v1h2.5V9zM2 12h2.5v-1H2v1zm9 1h1v2c-.02.28-.11.52-.3.7-.19.18-.42.28-.7.3H1c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h3c0-1.11.89-2 2-2 1.11 0 2 .89 2 2h3c.55 0 1 .45 1 1v5h-1V6H1v9h10v-2zM2 5h8c0-.55-.45-1-1-1H8c-.55 0-1-.45-1-1s-.45-1-1-1-1 .45-1 1-.45 1-1 1H3c-.55 0-1 .45-1 1z\"></path>\n    </svg>\n  </button>";
+
+var isLargeButton = exports.isLargeButton = function isLargeButton() {
+  var sites = /^(github.com|gist.github.com|medium.com|www.npmjs.com|developer.mozilla.org)$/;
+  return sites.exec(document.location.hostname);
+};
+
+},{}]},{},[9]);
